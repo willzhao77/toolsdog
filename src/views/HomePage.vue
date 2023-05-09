0e97077b07c903522a7dc0663ea5baa7c71e0f57
Will Zhao <will@publica.co.nz>
2023-05-10 09:20:19 +1200

    add arco design. config auto import.

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,10 +2,18 @@
     <div>
         hello, This is toolsdog home page!
     </div>
+    <a-space>
+    <a-button type="primary" size="mini" @click="handleClickMini">Mini</a-button>
+    <a-button type="primary" size="small">Small</a-button>
+    <a-button type="primary">Medium</a-button>
+    <a-button type="primary" size="large">Large</a-button>
+  </a-space>
 </template>
 
 <script setup>
-
+const handleClickMini = () => {
+  AMessage.info('hello isboyjc, click mini AButton!')
+};
 </script>
 
 <style scoped>
