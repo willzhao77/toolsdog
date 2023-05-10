43ecf1667634b1bcfea85408ab8e592976c5a90b
Will Zhao <will@publica.co.nz>
2023-05-10 23:15:07 +1200

    install UnoCSS

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -15,6 +15,24 @@
     <icon-raphael-bolt class="" />
     <icon-home-copy class=""/>
   </div>
+  <div>
+    <button class="bg-blue-400 hover:bg-blue-500 text-sm text-white font-mono font-light py-2 px-4 rounded border-2 border-blue-200 dark:bg-blue-500 dark:hover:bg-blue-600">
+      Button
+    </button>
+    <button 
+      bg="blue-400 hover:blue-500 dark:blue-500 dark:hover:blue-600"
+      text="sm white"
+      font="mono light"
+      p="y-2 x-4"
+      border="2 rounded blue-200"
+    >
+      Button
+    </button>
+    <button class="btn-style">
+      Button
+    </button>
+
+  </div>
 </template>
 
 <script setup>
@@ -24,5 +42,9 @@ const handleClickMini = () => {
 </script>
 
 <style scoped>
-
+.btn-style{
+  @apply bg-blue-400 text-sm text-white font-mono font-light py-2 px-4 rounded border-2 border-blue-200;
+  @apply hover:bg-blue-500;
+  @apply dark:bg-blue-500 dark:hover:bg-blue-600;
+}
 </style>
