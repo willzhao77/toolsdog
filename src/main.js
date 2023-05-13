da8363eaf90cab97a01ad279c02e4fb103a123ad
Will Zhao <will@publica.co.nz>
2023-05-13 15:44:23 +1200

    pinia persistedstate.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+// 引入 Pinia 状态持久化插件
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import "@/styles/normalize.css"
 
@@ -11,7 +13,13 @@ import router from './router'
 
 const app = createApp(App)
 
-app.use(createPinia())
+// 创建 Pinia 实例
+const pinia = createPinia()
+// 使用 Pinia 状态持久化插件
+pinia.use(piniaPluginPersistedstate)
+
+app.use(pinia)
+
 app.use(router)
 
 app.mount('#app')
--- src/stores/system.js
+++ src/stores/system.js
@@ -1,6 +1,9 @@
+import { getConfig } from '@/config/index'
+
 export const useSystemStore = defineStore('system', () => {
     // 当前可切换布局
     const currentSwitchlayout = shallowRef(null)
+
     // 可切换布局列表
     const switchLayoutList = shallowRef([])
   
@@ -11,7 +14,12 @@ export const useSystemStore = defineStore('system', () => {
   
         if (!currentSwitchlayout.value) {
           currentSwitchlayout.value = switchLayoutList.value[0]
-        }
+        }else {
+            // 通过name属性找到布局对象并赋值，因为持久化数据中没有组件渲染的render函数
+            currentSwitchlayout.value = switchLayoutList.value.find(
+              item => item.name === currentSwitchlayout.value.name
+            )
+          }
       }
     }
   
@@ -20,4 +28,21 @@ export const useSystemStore = defineStore('system', () => {
       switchLayoutList,
       initSwitchLayout
     }
-  })
+  },
+  // 新增第三个参数
+  /*
+    key 属性用来配置持久化时缓存数据的 key，默认是模块名。
+    enabled 属性代表是否开启持久化。
+    storage 属性可以配置如何进行持久化存储，可以写成 sessionStorage，默认是使用 localStorage ，所以这里我们其实不写也可以。
+    paths 属性即配置模块中需要做持久化的状态列表，不写就是默认缓存该模块中的全部状态。
+    serializer 此对象可以自定义序列化方法，默认使用 JSON.stringify/JSON.parse 做序列化
+  */
+  {
+    persist: {
+      key: `${getConfig('appCode')}-pinia-system`,
+      enabled: true,
+      storage: window.localStorage,
+      paths: ['currentSwitchlayout.name']
+    }
+  }
+)
